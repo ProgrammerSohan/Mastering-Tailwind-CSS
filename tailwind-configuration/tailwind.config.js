038bbe1a25a3c28e321a9aeef126e0e6830c206b
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-28 12:20:15 +0600

    12_overide options_t2_s4

--- tailwind-configuration/tailwind.config.js
+++ tailwind-configuration/tailwind.config.js
@@ -2,12 +2,22 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
+    /*
     extend: {
       colors: {
           'yellowColor': '#FFECB3'
       }
 
-    },
+    },*/
+    
+    screens:{ 
+      "short" : '400px',
+      'mobile' : '640px',
+      'tablet' : '768px',
+      'laptop' : '1024px'
+
+    }
+  
   },
   plugins: [],
 }
